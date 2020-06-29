a67c6be95381e1bd1b7f97e8fd2a41874bb2ea71
Test Git <710784360@qq.com>
2020-06-29 20:58:21 +0800

    11

--- src/test/java/org/kframe/SimpleSource.java
+++ src/test/java/org/kframe/SimpleSource.java
@@ -1,15 +1,15 @@
-package org.kframe;
-
-import com.alibaba.druid.pool.DruidDataSource;
-
-public class SimpleSource {
-	public static void main(String[] args) {
-		DruidDataSource source = new DruidDataSource();
-		String url = "";
-		String username = "";
-		String password = "";
-		String filters  = "";
-		String maxActive = "";
-
-	}
-}
+//package org.kframe;
+//
+//import com.alibaba.druid.pool.DruidDataSource;
+//
+//public class SimpleSource {
+//	public static void main(String[] args) {
+//		DruidDataSource source = new DruidDataSource();
+//		String url = "";
+//		String username = "";
+//		String password = "";
+//		String filters  = "";
+//		String maxActive = "";
+//
+//	}
+//}
---
+++ src/test/java/org/kframe/servlet/ServletSimple.java
@@ -0,0 +1,9 @@
+package org.kframe.servlet;
+
+import javax.servlet.AsyncContext;
+
+public class ServletSimple {
+
+    AsyncContext context =null;
+
+}
